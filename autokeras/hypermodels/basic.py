aa5941df0f1db425b5c5d823f8354788125018d1
Vlad Vergu <v.vergu@gmail.com>
2020-05-07 12:28:40 +0300

    Drop some DenseBlock customizations

--- autokeras/hypermodels/basic.py
+++ autokeras/hypermodels/basic.py
@@ -58,14 +58,14 @@ class DenseBlock(block_module.Block):
         #     noise_std = hp.Choice('gaussian_input_noise_std', [0.1, 0.2, 0.3], default=0.2)
         #     output_node = layers.GaussianNoise(noise_std)(output_node)
 
-        num_layers = self.num_layers or hp.Choice('num_layers', [1, 2, 3, 4, 5, 6], default=2)
+        num_layers = self.num_layers or hp.Choice('num_layers', [1, 2, 3, 4, 5], default=2)
         use_batchnorm = self.use_batchnorm
         if use_batchnorm is None:
             use_batchnorm = hp.Boolean('use_batchnorm', default=False)
         if self.dropout_rate is not None:
             dropout_rate = self.dropout_rate
         else:
-            dropout_rate = hp.Choice('dropout_rate', [0.0, 0.15, 0.25, 0.5], default=0)
+            dropout_rate = hp.Choice('dropout_rate', [0.0, 0.25, 0.5], default=0)
 
         for i in range(num_layers):
             units = hp.Choice(
